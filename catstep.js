09bd0026b372e02447e2eafba1697d5a2df49f1d
coderaiser <mnemonic.enemy@gmail.com>
2014-12-30 06:11:53 -0500

    feature(catstep) add up, down

--- catstep.js
+++ catstep.js
@@ -3,8 +3,10 @@
     
     var Cat         = getByDataName('catstep'),
         Direction   = {
-            right: getByDataNameAll('catstep-right'),
-            left: getByDataNameAll('catstep-left')
+            left    : getByDataNameAll('catstep-left'),
+            right   : getByDataNameAll('catstep-right'),
+            up      : getByDataNameAll('catstep-up'),
+            down    : getByDataNameAll('catstep-down'),
         },
         
         Move        = {
@@ -16,8 +18,10 @@
             Array.prototype.forEach.call(array, callback);
         },
         
+        KEY_LEFT    = 37,
         KEY_RIGHT   = 39,
-        KEY_LEFT    = 37;
+        KEY_UP      = 38,
+        KEY_DOWN    = 40;
     
     window.addEventListener('keydown', function(event) {
         switch (event.keyCode) {
@@ -27,22 +31,34 @@
         case KEY_LEFT:
             left();
             break;
+        case KEY_UP:
+            up();
+            break;
+        case KEY_DOWN:
+            down();
+            break;
         }
     });
     
     Move.right.addEventListener('click', right);
     Move.left.addEventListener('click', left);
     
-    function right() {
-        modify(Cat.style, 10);
-        step('right');
-        hide('left');
-    }
-    
     function left() {
         modify(Cat.style, -10);
         step('left');
-        hide('right');
+    }
+    
+    function right() {
+        modify(Cat.style, 10);
+        step('right');
+    }
+    
+    function up() {
+        step('up');
+    }
+    
+    function down() {
+        step('down');
     }
     
     function modify(style, number) {
@@ -51,16 +67,27 @@
     }
     
     function step(direction) {
-        var step1   = Direction[direction][0],
-            step2   = Direction[direction][1];
-        
-        if (!step1.hidden) {
-            step1.hidden = true;
-            step2.hidden = false;
-        } else {
-            step1.hidden = false;
-            step2.hidden = true;
-        }
+        Object.keys(Direction).forEach(function(where) {
+            var elements = Direction[where];
+            
+            if (where !== direction)
+                hide(where);
+            else
+                switch (elements.length) {
+                case 1:
+                    elements[0].hidden = false;
+                    break;
+                case 2:
+                    if (elements[0].hidden) {
+                        elements[0].hidden = false;
+                        elements[1].hidden = true;
+                    } else {
+                        elements[0].hidden = true;
+                        elements[1].hidden = false;
+                    }
+                    break;
+                }
+        });
     }
     
     function hide(direction) {
